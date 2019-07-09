73d12946c8b4dbec39c1c2931352b2a762db0c5f
cyriliser <siyathokozacmbatha@gmail.com>
2019-07-09 12:40:54 -0700

    fixed api, added mini api doc to /api

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -31,20 +31,21 @@ const add_post_p = (req,res) => {
     post.body = req.body.body;
     post.image = req.body.image;
 
-    post.save((err)=>{
+    post.save((err,post)=>{
         if (err){
             console.log(err);
             return;
         }
         else{
-            res.status(200);
+            res.status(201);
+            res.json(post);
             return;
         }
     });
 
 }
 
-const get_posts_g = (res,req)=>{
+const get_posts_g = (req,res)=>{
     Post.find({},(err,posts) => {
         if(err){
             console.log(err);
@@ -54,10 +55,41 @@ const get_posts_g = (res,req)=>{
     });
 }
 
+const api_g = (req, res) => {
+    res.json([
+        //api/
+        {
+            url: 'api/',
+            title: 'root of api',
+            methods: [{name: 'GET',purpose:'has details about api endpoints'}],
+        },
+        //api/posts
+        {
+            url: 'api/posts',
+            title: 'posts',
+            methods: [{name: 'GET',purpose:'for getting all the posts'}],
+        },
+        //api/posts/add
+        {
+            url: 'api/posts/add',
+            title: 'posts',
+            methods: [{name: 'POST',purpose:'Adding a new Post'}],
+            desc: "take in a json object with title,body,image and returns 201 status and the new object from the db"
+        },
+        //api/posts/:id
+        {
+            url: 'api/posts/:id',
+            title: 'post',
+            methods: [{name: 'GET',purpose:'for getting one post using id'}],
+        }
+    ]);
+}
+
 //mapping routes to functions
-router.get('/',get_posts_g)
-router.get('/:id',get_post_g);
-router.post('/add',add_post_p);
+router.get('/',api_g)
+router.get('/posts',get_posts_g)
+router.get('/posts/:id',get_post_g);
+router.post('/posts/add',add_post_p);
 //end of routes
 
 
