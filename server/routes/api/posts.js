7c003694168f8d5ea94109d7f7675f4e920b3e89
cyriliser <siyathokozacmbatha@gmail.com>
2019-07-10 23:04:24 -0700

    completed CRUD API, and CRUD Front End Template

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -11,6 +11,48 @@ const Post = require('../../models/post.js');
 
 //routes
 //route functions
+const api_g = (req, res) => {
+    res.json([
+        //api/
+        {
+            url: 'api/',
+            title: 'root of api',
+            methods: [{name: 'GET',purpose:'has details about api endpoints'}],
+        },
+        //api/posts
+        {
+            url: 'api/posts',
+            title: 'posts',
+            methods: [{name: 'GET',purpose:'for getting all the posts'}],
+        },
+        //api/posts/add
+        {
+            url: 'api/posts/add',
+            title: 'posts',
+            methods: [{name: 'POST',purpose:'Adding a new Post'}],
+            desc: "take in a json object with title,body,image and returns 201 status and the new object from the db"
+        },
+        //api/posts/:id
+        {
+            url: 'api/posts/:id',
+            title: 'post',
+            methods: [{name: 'GET',purpose:'for getting one post using id'}],
+        },
+        //api/posts/delete/:id
+        {
+            url: 'api/posts/delete/:id',
+            title: 'delete',
+            methods: [{name: 'DELETE',purpose:'for deleting one post using id'}],
+        },
+        //api/posts/update/:id
+        {
+            url: 'api/posts/update/:id',
+            title: 'Update',
+            methods: [{name: 'PUT',purpose:'for updating one post using id'}],
+        }
+    ]);
+}
+
 const get_post_g = (req, res) => {
     const id = req.params.id;
     Post.findById(id,(err,post)=>{
@@ -55,36 +97,6 @@ const get_posts_g = (req,res)=>{
     });
 }
 
-const api_g = (req, res) => {
-    res.json([
-        //api/
-        {
-            url: 'api/',
-            title: 'root of api',
-            methods: [{name: 'GET',purpose:'has details about api endpoints'}],
-        },
-        //api/posts
-        {
-            url: 'api/posts',
-            title: 'posts',
-            methods: [{name: 'GET',purpose:'for getting all the posts'}],
-        },
-        //api/posts/add
-        {
-            url: 'api/posts/add',
-            title: 'posts',
-            methods: [{name: 'POST',purpose:'Adding a new Post'}],
-            desc: "take in a json object with title,body,image and returns 201 status and the new object from the db"
-        },
-        //api/posts/:id
-        {
-            url: 'api/posts/:id',
-            title: 'post',
-            methods: [{name: 'GET',purpose:'for getting one post using id'}],
-        }
-    ]);
-}
-
 const delete_post_d = (req, res) =>{
     const id = req.params.id;
     Post.findByIdAndRemove(id)
@@ -101,7 +113,7 @@ const delete_post_d = (req, res) =>{
 }
 
 
-const edit_post_p = (req,res)=>{
+const update_post_pu = (req,res)=>{
     const id = req.params.id;
     const post = {};
     post.title = req.body.title;
@@ -124,7 +136,7 @@ router.get('/posts',get_posts_g)
 router.get('/posts/:id',get_post_g);
 router.post('/posts/add',add_post_p);
 router.delete('/posts/delete/:id',delete_post_d);
-router.put('/posts/update/:id',edit_post_p);
+router.put('/posts/update/:id',update_post_pu);
 //end of routes
 
 
